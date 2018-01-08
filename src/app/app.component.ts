6995169cc4ffc7a66f38e67112c56db18361e8ce
daliyajohn <daliyajohn75@gmail.com>
2018-01-08 17:48:19 +0530

    Implement reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup , FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  myForm: FormGroup;
+  constructor( public fb: FormBuilder ) {
+  }
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      name: ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])],
+      email : ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])],
+      address : ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])],
+      phone : ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])],
+      city : ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])],
+      state : ['',  Validators.compose([<any>Validators.required, <any>Validators.maxLength(30)])]
+    });
+    console.log(this.myForm);
+  }
+  submit(data: any) {
+    // code
+  }
 }
